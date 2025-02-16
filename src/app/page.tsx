0f2127039ba0b1ea61300a46176988243bffcdd5
NQ <121035503+neiruhitori@users.noreply.github.com>
2025-02-16 21:25:38 +0700

    Hello Word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,3 +116,4 @@ function MyButton() {
 }
 
 function Profile() { return ( <> <div className="text-xs">Developed by: {user.name}</div> <img className="rounded-full" src={user.imageUrl} alt={'Foto ' + user.name} style={{ width: user.imageSize, height: user.imageSize }} /> </> ); } const user = { name: 'Fulanah bin Fulan', imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg', imageSize: 90, };
+// p
