c7c7789f8f5dab8417f627e2edd84241fdad9e64
NQ <121035503+neiruhitori@users.noreply.github.com>
2025-02-16 21:28:52 +0700

    Hello Word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,4 +116,3 @@ function MyButton() {
 }
 
 function Profile() { return ( <> <div className="text-xs">Developed by: {user.name}</div> <img className="rounded-full" src={user.imageUrl} alt={'Foto ' + user.name} style={{ width: user.imageSize, height: user.imageSize }} /> </> ); } const user = { name: 'Fulanah bin Fulan', imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg', imageSize: 90, };
-// p
